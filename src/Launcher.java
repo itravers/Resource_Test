bd0cdda4834c99e729ac0678e9b0213b58407c31
Isaac Assegai <isaac.a.travers@gmail.com>
2016-02-11 18:08:10 -0800

    Planning Notes

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,4 +1,20 @@
-
+/**
+ * Launches Whatever test application you desire. (Currently only 1)
+ * @author Isaac Assegai
+ *
+ */
 public class Launcher {
+	public static void main(String [] args){
+		generationTest1();
+	}
+	
+	/**
+	 * For generationTest1 we will be making a resource generation system for a 2d Space Game.
+	 * This will be a basic animated swing application. 
+	 */
+	private static void generationTest1(){
+		
+		
+	}
 
 }
